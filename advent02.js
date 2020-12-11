86de5b47eafdd861ac1424dfd3cc156d24086284
Matt Cohen <cruisercohen@yahoo.com>
2020-12-11 20:30:06 +0000

    day  2 stub

---
+++ advent02.js
@@ -0,0 +1,7 @@
+
+
+const passCheck = () => {
+  console.log('to implment')
+}
+
+module.exports = {passCheck};
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const advent01 = require('./advent01');
+const advent02 = require('./advent02');
 
 advent01.findNums();
+advent02.passCheck();
